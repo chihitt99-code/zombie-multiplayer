0ef49e29f31865304a89cf45cbdfa32edb9d79d7
chihitt99-code <chihitt99@gmail.com>
2025-11-12 18:06:15 +0900

    갱신 프로젝트 재수정
    갱신 프로젝트 재수정

--- zombie-MultiPlayer/Assets/Woman.cs
+++ zombie-MultiPlayer/Assets/Woman.cs
@@ -21,27 +21,29 @@ public class Woman : MonoBehaviourPun
     void Update()
     {
 
-        if (!photonView.IsMine)
-            return;
-        
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        if (photonView.IsMine)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
            
-           Vector3 playerMove = new Vector3(h, 0, v);
-           if (playerMove != Vector3.zero)
-           {
-               Quaternion newRotation = Quaternion.LookRotation(playerMove,Vector3.up);
-               transform.rotation = newRotation;
-               transform.Translate(Vector3.forward * Time.deltaTime * speed);
-               animator.SetInteger("State",1);
+            Vector3 playerMove = new Vector3(h, 0, v);
+            if (playerMove != Vector3.zero)
+            {
+                Quaternion newRotation = Quaternion.LookRotation(playerMove,Vector3.up);
+                transform.rotation = newRotation;
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                animator.SetInteger("State",1);
                
               
-           }
-           else
-           {
-               animator.SetInteger("State",0);
-           }
+            }
+            else
+            {
+                animator.SetInteger("State",0);
+            }
 
+        }
+        
+       
        
         
             
