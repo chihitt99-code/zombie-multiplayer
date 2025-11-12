fdbf1363096966cfa2d62a0a758033a1227b67bf
chihitt99-code <chihitt99@gmail.com>
2025-11-12 15:36:53 +0900

    우먼 생성
    우먼 생성

---
+++ zombie-MultiPlayer/Assets/Woman.cs
@@ -0,0 +1,17 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class Woman : MonoBehaviourPun
+{
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
