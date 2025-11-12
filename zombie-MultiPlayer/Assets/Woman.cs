ff6086751bc0e39bbf97dee09e958a2666170247
chihitt99-code <chihitt99@gmail.com>
2025-11-12 17:24:43 +0900

    캐릭터 이동
    캐릭터 이동

--- zombie-MultiPlayer/Assets/Woman.cs
+++ zombie-MultiPlayer/Assets/Woman.cs
@@ -1,17 +1,54 @@
+using System.Numerics;
 using Photon.Pun;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class Woman : MonoBehaviourPun
 {
+    public float speed = 1f;
+    public float rotSpeed = 1f;
+    
+    private Animator animator;
+    private CharacterController characterController;
     
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+       
     }
 
    
     void Update()
     {
+
+        if (!photonView.IsMine)
+            return;
+        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+           
+           Vector3 playerMove = new Vector3(h, 0, v);
+           if (playerMove != Vector3.zero)
+           {
+               Quaternion newRotation = Quaternion.LookRotation(playerMove,Vector3.up);
+               transform.rotation = newRotation;
+               transform.Translate(Vector3.forward * Time.deltaTime * speed);
+               animator.SetInteger("State",1);
+               
+              
+           }
+           else
+           {
+               animator.SetInteger("State",0);
+           }
+
+       
+        
+            
+        
+        
         
     }
 }
