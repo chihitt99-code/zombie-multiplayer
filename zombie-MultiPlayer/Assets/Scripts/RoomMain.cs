eea4f59f1edb0293b741c7ea24a61eb0ac3b4d9d
chihitt99-code <chihitt99@gmail.com>
2025-11-13 16:47:11 +0900

    PhotonNetwork.CurrentRoom.PlayerCount 체크
    PhotonNetwork.CurrentRoom.PlayerCount 체크

--- zombie-MultiPlayer/Assets/Scripts/RoomMain.cs
+++ zombie-MultiPlayer/Assets/Scripts/RoomMain.cs
@@ -14,7 +14,9 @@ public class RoomMain : MonoBehaviour
     private void Start()
     {
         Debug.Log(PhotonNetwork.LocalPlayer);
-        Debug.Log(PhotonNetwork.PlayerList.Length);
+        Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount);
+        
+        //룸에 대한 정보를 받아올수는 없을까 ?
     }
  
 }
