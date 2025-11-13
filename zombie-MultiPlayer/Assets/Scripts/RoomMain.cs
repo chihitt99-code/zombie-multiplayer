a24d49c4db24a3fc88240bb418cb261eabfd40a4
chihitt99-code <chihitt99@gmail.com>
2025-11-13 17:02:25 +0900

    Room메인에서 방장과 나 알기
    Room메인에서 방장과 나 알기

--- zombie-MultiPlayer/Assets/Scripts/RoomMain.cs
+++ zombie-MultiPlayer/Assets/Scripts/RoomMain.cs
@@ -12,7 +12,26 @@ public class RoomMain : MonoBehaviour
         EventDispatcher.instance.AddEventHandler<Player>(
             (int)EventEnums.EventType.OnPlayerEnteredRoom,
             OnPlayerEnteredRoomEvent);
+        
+        EventDispatcher.instance.AddEventHandler(
+            (int)EventEnums.EventType.OnJoinedRoom,
+            OnJoinedRoomEvent);
  
+        
+    }
+
+
+    private void OnJoinedRoomEvent(short eventType)
+    {
+        var me = PhotonNetwork.LocalPlayer; //내꺼
+
+        // 나보다 먼저 들어와 있던 사람들 출력
+        foreach (var p in PhotonNetwork.PlayerList)
+        {
+            if (p == me) continue; //내꺼면 컨티뉴
+            Debug.Log($"[{p.NickName}]님이 입장 했습니다."); // 내가 아닌사람만 남음
+        }
+
     }
     
     private void OnPlayerEnteredRoomEvent(short eventType, Player newPlayer)
